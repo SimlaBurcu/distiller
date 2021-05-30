134d3af10a07e4124caf22eeb00726d5894047f7
SimlaBurcu <simlaharma@gmail.com>
2021-05-30 14:08:09 +0200

    a

--- distiller/tpr/tpr.py
+++ distiller/tpr/tpr.py
@@ -510,6 +510,7 @@ def test_float_to_fp4():
     rounding_mode = 'determ'
 
     numbers = [0.0,1.0,2.0,3.0,4.0,5.0,5.1,6.0,8.0,9.0,10.0,11.0,13.0,14.0,15.0,16.0,17.0,18.0,19.0,20.0,21.0,22.0,23.0,24.0,25.0,26.0,27.0,28.0,29.0,30.0,31.0,32.0]
+    numbers1 = [15.0,16.0,17.0,18.0,19.0,20.0,21.0,22.0,23.0,24.0,25.0,26.0,27.0,28.0,29.0,30.0,31.0,32.0,33.0,34.0,35.0,63.0,64.0]
     numbers2 = [0.0064, 0.00664, 0.01133, 0.5036, 0.3617, 0.43733, 0.09754, 0.1647]
     finaleven = []
     finalodd = []
@@ -542,7 +543,7 @@ def test_float_to_fp4():
 
 
 
-    x_data = torch.tensor(numbers)
+    x_data = torch.tensor(numbers1)
     e=tensortpr(x_data, epsilon, "even", device)
     print(f'even:{e}')
     o=tensortpr(x_data, epsilon, "odd", device)
