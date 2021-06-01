f6f1ab1b0150e959d11c0c05a1fdb33e89af86de
SimlaBurcu <simlaharma@gmail.com>
2021-06-01 13:32:46 +0200

    a

--- distiller/tpr/tpr.py
+++ distiller/tpr/tpr.py
@@ -440,6 +440,7 @@ def test_autograd():
 
         @staticmethod
         def backward(ctx, grad_output):
+            pdb.set_trace()
             print(f'tpr2 backward input:{grad_output}')
             input, weight = ctx.saved_tensors
             with torch.enable_grad():
