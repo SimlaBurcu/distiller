5e6af4a5e6143c66d247f98b7865e8c5c28ef0cf
SimlaBurcu <simlaharma@gmail.com>
2021-06-18 16:05:50 +0200

    a

--- distiller/tpr/tpr0.py
+++ distiller/tpr/tpr0.py
@@ -37,7 +37,7 @@ import logging
 import unittest
 import math
 import time
-from distiller.distiller.tpr.tpr_optim import *
+from distiller.tpr.tpr_optim import *
 
 def tensortpr2(tensor):
     """
